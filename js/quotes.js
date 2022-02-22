8036e2b5f2249ad4f5ea2d8151b7e15185045dc9
eeejihyeon <eeejihyeon@naver.com>
2022-02-22 15:11:06 +0900

    Add quotes

---
+++ js/quotes.js
@@ -0,0 +1,46 @@
+const quotes = [
+  {
+    quote:
+      "Spread love everywhere you go. Let no one ever come to you without leaving happier.",
+    author: "Mother Teresa",
+  },
+  {
+    quote:
+      "Always remember that you are absolutely unique. Just like everyone else.",
+    author: "Margaret Mead",
+  },
+  {
+    quote:
+      "The future belongs to those who believe in the beauty of their dreams.",
+    author: "Eleanor Roosevelt",
+  },
+  {
+    quote: "Whoever is happy will make others happy too.",
+    author: "Anne Frank",
+  },
+  {
+    quote:
+      "Life is a succession of lessons which must be lived to be understood.",
+    author: "Ralph Waldo",
+  },
+  {
+    quote: "Love the life you live. Live the life you love.",
+    author: "Bob Marley",
+  },
+  {
+    quote: "Life is made of ever so many partings welded together.",
+    author: "Charles Dickens",
+  },
+  {
+    quote: "Life is a long lesson in humility.",
+    author: "James M. Barrie",
+  },
+  {
+    quote: "Life is trying things to see if they work.",
+    author: "Ray Bradbury",
+  },
+  {
+    quote: "Dream big and dare to fail.",
+    author: "Norman Vaughan",
+  },
+];
