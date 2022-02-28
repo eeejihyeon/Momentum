87e9651456e3751929bab84f5a1a88bb817f1619
eeejihyeon <eeejihyeon@naver.com>
2022-03-01 00:20:47 +0900

    Login form JS/CSS update

--- js/m_greeting.js
+++ js/m_greeting.js
@@ -1,38 +1,27 @@
 const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginInput = document.querySelector("#login-form div input");
+const loginBtn = document.querySelector("#login-form input:last-child");
 const greeting = document.querySelector("#greeting");
+const container = document.querySelector(".container");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
-  /// JavaScript 이용한 유효성 검사
-  // const nameValue = loginInput.value;
-  // if (nameValue === "") {
-  //   alert("Please write your name.");
-  // } else if (nameValue.length > 15) {
-  //   alert("Your name is too long.");
-  // }
-
-  /// HTML에서 제한(required, maxlength...)을 지정했을 경우
-  /// preventDefault(); 로 submit 시 기본 동작인 새로고침을 실행하지 않도록 지정
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
 
-  // 문자열 표기법 1
-  // greeting.innerText = "Hello " + username + "!";
-  // 문자열 표기법 2
-  // greeting.innerText = `Hello ${username}!`;
-  // greeting.classList.remove(HIDDEN_CLASSNAME);
-
-  // 위 두줄을 function paintGreetings으로 저장했기 때문에 아래 코드 한줄로!
   paintGreetings(username);
 }
 
 function paintGreetings(username) {
   greeting.innerText = `Have a good day, ${username} !`;
+  loginForm.classList.remove("login-form");
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  container.classList.remove(HIDDEN_CLASSNAME);
+  container.classList.add("fadein");
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -47,7 +36,17 @@ if (savedUsername === null) {
   paintGreetings(savedUsername);
 }
 
-// localStorage: 작은 미니 DB 같은 API, key(username... )와 value 입력하면 브라우저가 기억(저장)
-// loacalStorage.setItem("key", "value")
-// loacalStorage.getItem("key");
-// loacalStorage.removeItem("key");
+function fadeout() {
+  loginForm.classList.remove("fadein");
+  loginForm.classList.add("fadeout");
+  loginForm.style.opacity = "0";
+  setTimeout(hide, 2000);
+}
+
+function hide() {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  container.classList.remove(HIDDEN_CLASSNAME);
+  container.classList.add("fadein");
+}
+
+loginBtn.addEventListener("click", fadeout);
