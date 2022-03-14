24efee3da0385a79076c92f44d0221d8ecadbaf8
eeejihyeon <eeejihyeon@naver.com>
2022-03-14 23:51:13 +0900

    add file - JS Challenge 03

---
+++ challenge/js/03.js
@@ -0,0 +1,9 @@
+const generateNbr = document.querySelector("#generateNbr");
+const guessNbr = document.querySelector("#guessNbr");
+const button = document.querySelector("button");
+
+function userChoseNumber() {}
+
+function machineChoseNumber() {}
+
+button.addEventListener("click", machineChoseNumber);
