9b01dcf51eb27a504ebee8e4ff0fe88fa81a05d2
eeejihyeon <eeejihyeon@naver.com>
2022-03-15 18:49:46 +0900

    JS Challenge 03

--- challenge/js/03.js
+++ challenge/js/03.js
@@ -1,46 +1,36 @@
-const body = document.body;
 const inputForm = document.querySelector("#inputForm");
 const generateNbr = document.querySelector("#generateNbr");
 const guessNbr = document.querySelector("#guessNbr");
 const button = document.querySelector("button");
+const h3 = document.querySelector("h3");
 
 const span = document.createElement("span");
-const h3 = document.createElement("h3");
+
+const HIDDEN_CLASS = "hidden";
 
 function userChoseNumber(event) {
-  // 1. 버튼 누르면 guessNbr에 유저가 입력한 숫자 출력
-  //    - localStorage와 preventDefault ?
-  //      (입력한 데이터 저장, 새로고침 방지)
   event.preventDefault();
 
-  // 2. 랜덤으로 0부터 generateNbr에 입력한 사이의 숫자 출력
-  // 3. 유저가 숫자 선택하지 않으면 게임을 플레이할 수 없음
-  // 4. generateNbr에 입력한 숫자보다 usernumber가 크면 안 됨
   const setnumber = generateNbr.value;
   const usernumber = guessNbr.value;
   const parseSetnumber = parseInt(setnumber);
   const parseUsernumber = parseInt(usernumber);
-
-  const random = Math.floor(Math.random() * setnumber);
+  const random = Math.round(Math.random() * parseSetnumber);
 
   if (usernumber === "" || setnumber === "") {
     alert("Please write number");
   } else if (parseUsernumber > parseSetnumber) {
     alert(`Please write between 0 and ${setnumber}`);
   } else {
-    inputForm.appendChild(span);
     span.innerText = `You chose: ${usernumber}, the machine chose: ${random}`;
-  }
+    inputForm.appendChild(span);
 
-  if (usernumber === random) {
-    h3.innerText = `You won!`;
-    inputForm.appendChild(h3);
-  } else {
-    console.log(`You lost!`);
+    if (parseUsernumber === random) {
+      h3.innerText = "You won!";
+    } else {
+      h3.innerText = "You lost!";
+    }
   }
-
-  // 5. 1과 2가 동일하다면 You won!, 아니라면 You lost! 출력
-  //    - if else ?
 }
 
 inputForm.addEventListener("submit", userChoseNumber);
