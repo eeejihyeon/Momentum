9a0ba1eafef206531ce97b9c1b181a32503fddb0
eeejihyeon <eeejihyeon@naver.com>
2022-03-18 16:11:50 +0900

    D-day update

--- js/dday.js
+++ js/dday.js
@@ -1,6 +1,20 @@
 const ddayFrom = document.querySelector("#dday-form");
 const ddayInput = ddayFrom.querySelector("input");
+const p = ddayFrom.querySelector("p");
 
-function getDday() {}
+function getDday() {
+  const inputValue = ddayInput.value;
+
+  const setDay = new Date(inputValue);
+  const today = new Date();
+  const dday = setDay - today;
+
+  const day = Math.floor(dday / (1000 * 60 * 60 * 24));
+
+  ddayInput.classList.add("hidden");
+  p.classList.remove("hidden");
+
+  p.innerText = `${setDay.toDateString()} - ${day}day`;
+}
 
 ddayInput.addEventListener("change", getDday);
